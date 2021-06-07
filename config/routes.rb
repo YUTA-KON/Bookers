01021adfa9cffa2d20ceb293d3cca5dfa6584381
YUTA-KON <dorata.1007@gmail.com>
2021-06-07 21:50:17 +0900

    [Finish]3-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top'
+  # resources :books
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  get 'books/:id' => 'books#show', as: 'book'
+  patch 'books/:id' => 'books#update'
+  put 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
 end
